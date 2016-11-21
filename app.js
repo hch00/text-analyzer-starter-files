cf75d6489d4ba33e7afc57c16592ce95183ad149
Harsh-Choudhary-01 <harshch000@gmail.com>
2016-11-21 09:55:44 -0600

    Finished

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// your code here!
+$(document).ready(function()
+{
+	$('form').submit(function(event)
+	{
+		processText($('textarea').val());
+		event.preventDefault();
+	})
+})
+function processText(text)
+{
+	var sentences = text.split('.');
+	var total = 0;
+	for(var i = 0; i < sentences.length ; i++)
+	{
+		total += sentences[i].length;
+	}
+	$('.js-sentence').text(total / sentences.length);
+	var words = text.split(' ');
+	var uniqueWords = {};
+	total = 0;
+	for(var i = 0; i < words.length ; i++)
+	{
+		total += words[i].length;
+		uniqueWords[words[i]] = 1;
+	}
+	$('.js-word-length').text(total / words.length);
+	$('.js-word-count').text(words.length);
+	$('.js-unique').text(Object.keys(uniqueWords).length);
+	$('dl').removeClass('hidden');
+}
